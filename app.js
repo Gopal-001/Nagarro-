0409cc6174bb0ddfbc64999c25cb03e914602529
Gopal-001 <sameeemishra@gmail.com>
2022-07-14 12:22:29 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-var list = ["sameer"];
+var list = [];
 let print = (list) => {
     const ul = document.getElementById('list');
     ul.innerHTML="";
-    // list.forEach(e => {
-    //     const cur = document.createElement('li');
-    //     base.appendChild(cur);
-    // });
     var cnt = 0;
     list.forEach(e => {
         const li = document.createElement("li");
         li.id = cnt;
-        let cur =  `<input type="checkbox" name="item" onchange="handleChange(this)" >
+        let cur =  `<input type="checkbox" name="item" onchange="handleChange(this,${cnt})" >
         <button class="common" type = "submit" onclick="delEle(${cnt})" title="Remove">
         <strong> - </strong></button> <button id="edit" type = "submit" onclick="makeEditable(${cnt})"
         title="Edit">
-        <strong> \\ </strong></button> <input type="text" class="listItem" value='${e}' id="i${cnt}" readonly>`;
+        <strong> \\ </strong></button> <input type="text" class="listItem" value='${e.text}' id="i${cnt}" readonly>`;
         li.innerHTML=cur;
+        if(e.val == true){
+            li.querySelector("input[type='checkbox']").checked=true;
+            li.querySelector("input[type='text']").style.textDecoration='line-through';
+        }
+        console.log(e);
         cnt++;
         ul.appendChild(li);
     });    
@@ -23,8 +24,9 @@ let print = (list) => {
 
 function makeEditable(edit){
     const input = document.querySelector('#i'+edit);
-    input.color = "black";
+    input.style.color = "orange";
     input.readOnly=false;
+    console.log(list[edit]);
     input.focus();
     input.addEventListener('keyup',e=>{
         if(e.keyCode == 13){
@@ -33,35 +35,38 @@ function makeEditable(edit){
                 input.value = "sorry task is empty";
                 input.readOnly=true;
             }else{
-                list[edit] = input.value;
+                list[edit].text = input.value;
                 print(list);
             }
         }
     });
+    
+    input.color = "black";
+    input.style.border="1px solid blue";
 }
 
 
 
-function handleChange(che){
-    let li = document.querySelector('#listItem');
+function handleChange(che,cnt){
+    let li = document.querySelector('#i'+cnt);
     if(che.checked){
         li.style.textDecoration  = "line-through";
     }else{
         li.style.textDecoration  = "none";
     }
-    
+    list[cnt].val = che.checked;
     // li.style.textDecoration  = "line-through";
     console.log(che.style );
 }
 
 function addNewItem(){
-    const val = document.getElementById('read').value;
-    if(val.trim() == ""){
+    const value = document.getElementById('read').value;
+    if(value.trim() == ""){
         document.getElementById('detail').style.color="red";
         document.getElementById('detail').innerHTML="sorry the task is empty!!!";
         return;
     }
-    list.push(val);
+    list.push({text:value,val:false});
     document.getElementById('read').value='';
     print(list);
     console.log(list);
